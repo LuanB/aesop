6223d7c95601b82f13303bc0a5b860f0602127e1
Luan Bui <luanb3455@gmail.com>
2020-06-20 11:10:55 +0800

    fetch data, useEffect to load data into state on component mount

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ function App() {
   const [hasError, setErrors] = useState(false);
   const [products, setProducts] = useState({});
 
+  async function fetchData() {
+    const res = await fetch("https://www.aesop.com/au/api/v1/nav/shop");
+    res
+      .json()
+      .then(res => setProducts(res))
+      .catch(err => setErrors(err));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
