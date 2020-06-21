0b985e5aafb434c8dd258c5f275805e1b2632603
Luan Bui <luanb3455@gmail.com>
2020-06-21 11:11:38 +0800

    restructuring folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 
-import Accordion from './Accordion';
-import Card from './Card';
+import Accordion from './components/Accordion';
+import Card from './components/Card';
 import './App.css';
 
 function App() {
